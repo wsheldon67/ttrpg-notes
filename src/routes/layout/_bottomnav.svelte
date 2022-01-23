2531ee25ceff63d69958a0e3100cce16fc529ce8
wsheldon67 <ws.livelaughlove@gmail.com>
2022-01-23 15:34:24 -0600

    folderize

---
+++ src/routes/layout/_bottomnav.svelte
@@ -0,0 +1,16 @@
+<script>
+  import {Nav, NavItem, Icon, Button, Styles} from 'sveltestrap'
+</script>
+
+<Styles />
+<Nav class="fixed-bottom justify-content-start" dark color="dark">
+  <NavItem class="ms-1">
+    <Button><Icon name="alarm" /></Button>
+  </NavItem>
+  <NavItem class="ms-1">
+    <Button><Icon name="map" /></Button>
+  </NavItem>
+  <NavItem class="ms-1">
+    <Button><Icon name="book" /></Button>
+  </NavItem>
+</Nav>
---
+++ src/routes/layout/_topnav.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Collapse, Navbar, NavbarBrand, NavbarToggler, Input, Form } from 'sveltestrap'
+  import SiteNav from './_sitenav.svelte'
+
+  let isOpen = false
+  function toggle() {isOpen = !isOpen}
+</script>
+
+<Navbar color="dark" dark class="fixed-top">
+  <NavbarToggler on:click={toggle} />
+  <Form class="flex-grow-1 mx-1">
+    <Input type="search" placeholder="search" />
+  </Form>
+  <NavbarBrand href="/" class="ms-2">Logo</NavbarBrand>
+  <Collapse {isOpen} navbar>
+    <SiteNav />
+  </Collapse>
+</Navbar>
