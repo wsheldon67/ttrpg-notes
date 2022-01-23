81ed218b3395cb592e4e477eb9345861bac79992
wsheldon67 <ws.livelaughlove@gmail.com>
2022-01-23 14:17:23 -0600

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.scss';
+  import { Collapse, Navbar, NavbarBrand, NavbarToggler, Input, Form } from 'sveltestrap'
+  import SiteNav from './_sitenav.svelte'
+
+  let isOpen = false
+  function toggle() {isOpen = !isOpen}
+</script>
+
+<Navbar color="dark" dark>
+  <NavbarToggler on:click={toggle} />
+  <Form class="flex-grow-1 mx-1">
+    <Input type="search" placeholder="search" />
+  </Form>
+  <NavbarBrand href="/" class="ms-2">Logo</NavbarBrand>
+  <Collapse {isOpen} navbar>
+    <SiteNav />
+  </Collapse>
+</Navbar>
+
+<slot />
---
+++ src/routes/_sitenav.svelte
@@ -0,0 +1,9 @@
+<script>
+  import {Nav, NavItem, NavLink} from 'sveltestrap'
+</script>
+
+<Nav navbar>
+  <NavItem>
+    <NavLink href='./notes'>Notes</NavLink>
+  </NavItem>
+</Nav>
