cca3ceabcb1f28ab58783d59a253cb2124d4c3a3
wsheldon67 <ws.livelaughlove@gmail.com>
2022-01-29 19:04:21 -0600

    db access

--- src/lib/_db.js
+++ src/lib/_db.js
@@ -1,7 +1,6 @@
 
 export async function find(url) {
   const res = await fetch(`/notes/all`,{method: 'POST'})
-  console.log(res)
   const txt = await res.json()
   if (res.ok) {return txt}
   else {throw new Error(txt)}
