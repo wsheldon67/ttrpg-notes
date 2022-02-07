25099af99536a65b62a45df142ed5600bc125ff7
wsheldon67 <ws.livelaughlove@gmail.com>
2022-02-07 15:20:18 -0600

    Create User

---
+++ src/routes/auth/login.svelte
@@ -0,0 +1,15 @@
+<script>
+  let username
+
+  function submit() {
+
+  }
+  // TODO login
+  // TODO create campaign
+  // TODO select campaign
+  // TODO store user/campaign in cookie, send w/ every req
+</script>
+<form on:submit|preventDefault={submit}>
+  <input placeholder='username' bind:value={username} />
+  <button type='submit'>Log In</button>
+</form>
---
+++ src/routes/auth/register.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { goto } from "$app/navigation";
+  import { find } from "$lib/_db";
+
+  let username
+
+  function submit(){
+    // TODO make more robust for unallowed usernames, implement server side as well
+    if (username === ''){return}
+    find('/auth/register',username)
+    .then((res) => {
+      goto('/')
+    })
+  }
+  // TODO notify/not allow when user already exists.
+</script>
+<form on:submit|preventDefault={submit}>
+  <input placeholder='username' bind:value={username}/>
+  <button type='submit'>Submit</button>
+</form>
