c6a0d09e7e4ac44db6c27ba440ff1311e7a00b6a
wsheldon67 <ws.livelaughlove@gmail.com>
2022-02-10 09:36:40 -0600

    split large time.js file

---
+++ src/lib/stores/time/settle.js
@@ -0,0 +1,38 @@
+import { settings } from '../settings'
+import { get } from 'svelte/store'
+
+
+export function settle(ob) {
+  const {
+    months_in_year,
+    days_in_month,
+    hours_in_day
+  } = get(settings).time
+
+  const units = {
+    'second': 60,
+    'minute': 60,
+    'hour': hours_in_day,
+    'day': days_in_month,
+    'month': months_in_year,
+    'year': Infinity
+  }
+
+  const sob = {second: 0}
+  let carry_over = 0
+
+  for (let unit in units) {
+    const overflow = units[unit]
+    let set_value = ob[unit] + carry_over
+
+    if (set_value >= overflow || set_value < 0) {
+      carry_over = Math.floor(set_value / overflow)
+      set_value -= carry_over * overflow
+    } else {
+      carry_over = 0
+    }
+    sob[unit] = Number(set_value)
+  }
+
+  return sob
+}
