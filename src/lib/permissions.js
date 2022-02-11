53c7e3be3f9382e67a7d9cb9acf92668fa527896
wsheldon67 <ws.livelaughlove@gmail.com>
2022-02-11 17:32:59 -0600

    Send role with note

--- src/lib/permissions.js
+++ src/lib/permissions.js
@@ -3,11 +3,17 @@ import { get } from "svelte/store";
 import { parse } from "./db/cookie";
 
 export function permission(key) {
-  // FIXME maybe do this as a derived store?
   const campaign = get(store)
   if (typeof(document) === 'undefined'){return}
   const user = parse(document.cookie).user
-  const role = campaign.users.find(el => el.user === user).role
-  if (role === 'owner'){return true}
-  return campaign.roles[role][key]
+  const srole = campaign.users.find(el => el.user === user).role
+  if (srole === 'owner'){return true}
+  return campaign.roles[srole][key]
+}
+
+export function role() {
+  const campaign = get(store)
+  if (typeof(document) === 'undefined'){return}
+  const user = parse(document.cookie).user
+  return campaign.users.find(el => el.user === user).role
 }
--- src/routes/app/notes/_QuickNote.svelte
+++ src/routes/app/notes/_QuickNote.svelte
@@ -1,10 +1,11 @@
 <script>
   import { post } from "$lib/db/client";
+  import { role } from "$lib/permissions";
 
   let note
   function keydown(e){
     if (e.key !== 'Enter'){return}
-    post('/app/notes/create', {body: note, type: 'quick'})
+    post('/app/notes/create', {body: note, type: 'quick', role: role()})
     .then(() => note = '')
   }
 </script>
