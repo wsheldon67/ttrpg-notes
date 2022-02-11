428efd3b7d1a0407c2b1430cf4b712619bbf5bde
wsheldon67 <ws.livelaughlove@gmail.com>
2022-02-11 12:28:53 -0600

    split app from auth

--- src/routes/app/__layout.svelte
+++ src/routes/app/__layout.svelte
@@ -3,7 +3,7 @@
   import TopNav from './_layout/topnav.svelte'
   import BottomNav from './_layout/bottomnav.svelte'
   import { onMount } from 'svelte'
-  import { onload } from './_onload';
+  import { onload } from '../.../../_onload';
   onMount(onload)
 </script>
 
