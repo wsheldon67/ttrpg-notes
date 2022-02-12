399a7df8235b5384280214e4f691640ed3ddf700
wsheldon67 <ws.livelaughlove@gmail.com>
2022-02-12 12:26:42 -0600

    forgot to set query

--- src/routes/app/notes/all.js
+++ src/routes/app/notes/all.js
@@ -5,9 +5,11 @@ export async function post (r) {
   return col('notes',r).find( async ({user, _id}) => {
     const view_gm_notes = await permission(user, _id, 'view_gm_notes')
     if (view_gm_notes) {
-      return {campaign: _id}
+      return {query: {campaign: _id}}
     } else {
-      return {campaign: _id, role: 'player'}
+      return {
+        query: {campaign: _id, role: 'player'}
+      }
     }
   })
 }
