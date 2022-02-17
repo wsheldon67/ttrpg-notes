00b6893f4d330998f7657cd456a23e39f8dacf60
wsheldon67 <ws.livelaughlove@gmail.com>
2022-02-16 22:13:32 -0600

    More opinionated for use in ObjectViewer

---
+++ src/lib/c/Collapse.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let collapsed = true
+  export let label = 'Please define a label for this Collapse.'
+
+  let className = ''
+
+  function toggle_collapse() {
+    collapsed = !collapsed
+    className = className === '' ? 'open' : ''
+    console.log(className)
+  }
+</script>
+<style>
+  button {
+    width: 100%;
+  }
+  div.open {
+    border: 1px solid var(--b4);
+    margin: 0em .25em;
+  }
+  button.open {
+    background-color: var(--b4);
+  }
+</style>
+<div class={className}>
+  <button on:click={toggle_collapse} class={className}>{label}</button>
+  {#if !collapsed}
+    <slot />
+  {/if}
+</div>
